f4a143085b39710c1cddd181ded0abb2b52fb89d
Amo Moloko <u14284783@tuks.co.za>
2017-06-27 16:28:10 +0200

    UserType
    Added The User Type CRUD

---
+++ TRWLASystemMaster/TRWLASystemMaster/Controllers/UserTypesController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using TRWLASystemMaster.Models.Usertype;
+
+namespace TRWLASystemMaster.Controllers
+{
+    public class UserTypesController : Controller
+    {
+        private Db_TRWLA_StagingEntities db = new Db_TRWLA_StagingEntities();
+
+        // GET: UserTypes
+        public ActionResult Index()
+        {
+            return View(db.UserTypes.ToList());
+        }
+
+        // GET: UserTypes/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            UserType userType = db.UserTypes.Find(id);
+            if (userType == null)
+            {
+                return HttpNotFound();
+            }
+            return View(userType);
+        }
+
+        // GET: UserTypes/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: UserTypes/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "UserTypeID,Description,AccessRight")] UserType userType)
+        {
+            if (ModelState.IsValid)
+            {
+                db.UserTypes.Add(userType);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(userType);
+        }
+
+        // GET: UserTypes/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            UserType userType = db.UserTypes.Find(id);
+            if (userType == null)
+            {
+                return HttpNotFound();
+            }
+            return View(userType);
+        }
+
+        // POST: UserTypes/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "UserTypeID,Description,AccessRight")] UserType userType)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(userType).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(userType);
+        }
+
+        // GET: UserTypes/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            UserType userType = db.UserTypes.Find(id);
+            if (userType == null)
+            {
+                return HttpNotFound();
+            }
+            return View(userType);
+        }
+
+        // POST: UserTypes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            UserType userType = db.UserTypes.Find(id);
+            db.UserTypes.Remove(userType);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
