8da4560af2dd594e9416229010fb4d0c0fab29cc
Amo Moloko <u14284783@tuks.co.za>
2017-08-21 17:44:43 +0200

    Data Annotations :knife:
    Added Data annnotations to the student class

--- TRWLASystemMaster/TRWLASystemMaster/Models/Student.cs
+++ TRWLASystemMaster/TRWLASystemMaster/Models/Student.cs
@@ -10,19 +10,48 @@
 namespace TRWLASystemMaster.Models
 {
     using System;
+    using System.ComponentModel.DataAnnotations;
     using System.Collections.Generic;
     
     public partial class Student
     {
         public int StudentID { get; set; }
+       [Required]
+        [StringLength(10, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+        [Display(Name = "Student number")]
         public string StudentNumber { get; set; }
+
         public string Graduate { get; set; }
+        [Required]
+        [StringLength(35, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+        [Display(Name = "Degree")]
         public string Degree { get; set; }
+
+         [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy/MM/dd}", ApplyFormatInEditMode = true)]
         public System.DateTime YearOfStudy { get; set; }
+
+        [Required]
+        [StringLength(35, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+        [Display(Name = "Student Name")]
         public string Student_Name { get; set; }
+
+        [Required]
+        [StringLength(35, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+        [Display(Name = "Student surname")]
         public string Student_Surname { get; set; }
+
+        [Required]
+        [StringLength(10, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+        [Display(Name = "Student phone number")]
         public string Student_Phone { get; set; }
+
+
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy/MM/dd}", ApplyFormatInEditMode = true)]
         public System.DateTime Student_DoB { get; set; }
+
+   
         public string ActiveStatus { get; set; }
         public int Id { get; set; }
         public int ResID { get; set; }
