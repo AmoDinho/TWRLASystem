f81e0e9bb2eefbf0cc11f83323572190476a730e
Amo Moloko <u14284783@tuks.co.za>
2017-08-21 17:49:31 +0200

    Data Annotations :fire:
    Added data annotations for student typey, venues and user types

--- TRWLASystemMaster/TRWLASystemMaster/Models/StudentType.cs
+++ TRWLASystemMaster/TRWLASystemMaster/Models/StudentType.cs
@@ -11,7 +11,8 @@ namespace TRWLASystemMaster.Models
 {
     using System;
     using System.Collections.Generic;
-    
+    using System.ComponentModel.DataAnnotations;
+
     public partial class StudentType
     {
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
@@ -21,6 +22,10 @@ namespace TRWLASystemMaster.Models
         }
     
         public int StudentTypeID { get; set; }
+
+        [Required]
+        [StringLength(25, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+        [Display(Name = "Student Type Description")]
         public string StudentTypeDescription { get; set; }
     
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
--- TRWLASystemMaster/TRWLASystemMaster/Models/Venue.cs
+++ TRWLASystemMaster/TRWLASystemMaster/Models/Venue.cs
@@ -11,7 +11,8 @@ namespace TRWLASystemMaster.Models
 {
     using System;
     using System.Collections.Generic;
-    
+    using System.ComponentModel.DataAnnotations;
+
     public partial class Venue
     {
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
