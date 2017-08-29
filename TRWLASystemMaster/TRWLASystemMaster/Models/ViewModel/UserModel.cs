32c0717a3c2dbd65a25efccc5c40ab082b1812a7
Amo Moloko <u14284783@tuks.co.za>
2017-08-29 21:58:28 +0200

    Registe Part I :8ball:
    Part of the resigertaion but the web config wont allow me to add a key part :dog:

---
+++ TRWLASystemMaster/TRWLASystemMaster/Models/ViewModel/UserModel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace TRWLASystemMaster.Models.ViewModel
+{
+    public class UserSignUpView
+    {
+        [Key]
+        public int SYSUserID { get; set; }
+        public int LOOKUPRoleID { get; set; }
+        public string RoleName { get; set; }
+        [Required(ErrorMessage = "*")]
+        [Display(Name = "Login ID")]
+        public string LoginName { get; set; }
+        [Required(ErrorMessage = "*")]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+        [Required(ErrorMessage = "*")]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+        [Required(ErrorMessage = "*")]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        public int UserTypeID { get; set; }
+
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+        public DateTime DoB { get; set; }
+
+        [Display(Name = "Phone Number")]
+        public string Phonenumber { get; set; }
+    }
+}
