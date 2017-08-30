f45299ebb01445437792ed352c093a20a4b11e5c
Amo Moloko <u14284783@tuks.co.za>
2017-08-30 17:58:38 +0200

    :last_quarter_moon_with_face:
    :stuck_out_tongue_closed_eyes:

--- TRWLASystemMaster/TRWLASystemMaster/Models/EntityManager/UserManager.cs
+++ TRWLASystemMaster/TRWLASystemMaster/Models/EntityManager/UserManager.cs
@@ -175,6 +175,114 @@ namespace TRWLASystemMaster.Models.EntityManager
         }
 
 
+        public List < LOOKUPAvailableRole > GetAllRoles()
+        {
+            using (TWRLADB_Staging_V2Entities17 db = new TWRLADB_Staging_V2Entities17())
+            {
+                var roles = db.LOOKUPRoles.Select(o => new LOOKUPAvailableRole
+                {
+                    LOOKUPRoleID = o.LOOKUPRoleID,
+                    RoleName = o.RoleName,
+                    RoleDescription = o.RoleDescription
+                }).ToList();
+
+                return roles;
+            }
+        }
+
+        public int GetUserID(string loginName)
+        {
+            using (TWRLADB_Staging_V2Entities17 db = new TWRLADB_Staging_V2Entities17())
+            {
+                var user = db.SYSUsers.Where(o => o.LoginName.Equals(loginName));
+                if (user.Any()) return user.FirstOrDefault().SYSUserID;
+            }
+            return 0;
+        }
+
+        public List<UserProfileView> GetAllUserProfiles()
+        {
+            List < UserProfileView > profiles = new List < UserProfileView > ();
+
+            using (TWRLADB_Staging_V2Entities17 db = new TWRLADB_Staging_V2Entities17())
+            {
+                UserProfileView UPV;
+                var users = db.SYSUsers.ToList();
+
+                foreach (SYSUser u in db.SYSUsers)
+                {
+                    UPV = new UserProfileView();
+                    UPV.SYSUserID = u.SYSUserID;
+                    UPV.LoginName = u.LoginName;
+                    UPV.Password = u.PasswordEncryptedText;
+
+                    var SUP = db.SYSUserProfiles.Find(u.SYSUserID);
+                    if (SUP != null)
+                    {
+                        UPV.FirstName = SUP.FirstName;
+                        UPV.LastName = SUP.LastName;
+                        UPV.Gender = SUP.Gender;
+                    }
+
+                    var SUR = db.SYSUserRoles.Where(o => o.SYSUserID.Equals(u.SYSUserID));
+                    if (SUR.Any())
+                    {
+                        var userRole = SUR.FirstOrDefault();
+                        UPV.LOOKUPRoleID = userRole.LOOKUPRoleID;
+                        UPV.RoleName = userRole.LOOKUPRole.RoleName;
+                        UPV.IsRoleActive = userRole.IsActive;
+                    }
+
+                    profiles.Add(UPV);
+                }
+            }
+
+            return profiles;
+        }
+
+        public UserDataView GetUserDataView(string loginName)
+        {
+            UserDataView UDV = new UserDataView();
+            List < UserProfileView > profiles = GetAllUserProfiles();
+            List <LOOKUPAvailableRole > roles = GetAllRoles();
+
+            int? userAssignedRoleID = 0, userID = 0;
+            string userGender = string.Empty;
+
+            userID = GetUserID(loginName);
+            using (TWRLADB_Staging_V2Entities17 db = new TWRLADB_Staging_V2Entities17())
+            {
+                userAssignedRoleID = db.SYSUserRoles.Where(o => o.SYSUserID == userID)?.FirstOrDefault().LOOKUPRoleID;
+                userGender = db.SYSUserProfiles.Where(o => o.SYSUserID == userID)?.FirstOrDefault().Gender;
+            }
+
+            List <Gender> genders = new List< Gender > ();
+            genders.Add(new Gender
+            {
+                Text = "Male",
+                Value = "M"
+            });
+            genders.Add(new Gender
+            {
+                Text = "Female",
+                Value = "F"
+            });
+
+            UDV.UserProfile = profiles;
+            UDV.UserRoles = new UserRoles
+            {
+                SelectedRoleID = userAssignedRoleID,
+                UserRoleList = roles
+            };
+
+            UDV.UserGender = new UserGender
+            {
+                SelectedGender = userGender,
+                Gender = genders
+            };
+
+            return UDV;
+        }
 
 
 
@@ -191,8 +299,7 @@ namespace TRWLASystemMaster.Models.EntityManager
 
 
 
-
- }
+    }
 
     
 }
