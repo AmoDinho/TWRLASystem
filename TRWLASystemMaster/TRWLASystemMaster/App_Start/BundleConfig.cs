b49ebe74a0843abe40c274c7921719a85baba5f5
Cailin <sheisbean@gmail.com>
2017-09-01 20:37:14 +0200

    updated css & video
    Made changes to css in navbar, icons and colours.
    
    Added facebook video

--- TRWLASystemMaster/TRWLASystemMaster/App_Start/BundleConfig.cs
+++ TRWLASystemMaster/TRWLASystemMaster/App_Start/BundleConfig.cs
@@ -8,9 +8,13 @@ namespace TRWLASystemMaster
         // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
+            bundles.Clear();
+            bundles.ResetAll();
+            BundleTable.EnableOptimizations = true;
+
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
-
+           
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
 
@@ -28,10 +32,12 @@ namespace TRWLASystemMaster
                       "~/Scripts/nouislider.min.js",
                       "~/Scripts/respond.js"));
 
-            bundles.Add(new StyleBundle("~/Content/css").Include(
+            bundles.Add(new StyleBundle("~/Content/lib").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/now-ui-kit.css",
-                      "~/Content/site.css",
+                      "~/Content/site.css"));
+
+            bundles.Add(new StyleBundle("~/Content/Custom").Include(
                       "~/Content/Custom.css"));
         }
     }
