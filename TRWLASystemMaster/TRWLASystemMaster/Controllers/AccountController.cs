00362731884818c4c4e686b5f3ea2cf5d2101768
Amo Moloko <u14284783@tuks.co.za>
2017-09-03 23:09:03 +0200

    :sa:

--- TRWLASystemMaster/TRWLASystemMaster/Controllers/AccountController.cs
+++ TRWLASystemMaster/TRWLASystemMaster/Controllers/AccountController.cs
@@ -53,9 +53,9 @@ namespace TRWLASystemMaster.Controllers
         public ActionResult Register(UserSignUpView USV)
         {
 
-            //try
-            //{
-            //    if (ModelState.IsValid)
+            try
+            {
+               if (ModelState.IsValid)
                 {
                     UserManager UM = new UserManager();
                     if (!UM.IsLoginNameExist(USV.LoginName))
@@ -75,13 +75,13 @@ namespace TRWLASystemMaster.Controllers
                         ModelState.AddModelError("", "Login Name already taken.");
                 }
                 return View();
-          //  }
+           }
 
             
-           ////catch (Exception ex)
-           //// {
-           //     return View("Error", new HandleErrorInfo(ex, "Account", "Register"));
-           //// }
+           catch (Exception ex)
+           {
+                return View("Error", new HandleErrorInfo(ex, "Account", "Register"));
+          }
         }
 
 
@@ -101,8 +101,8 @@ namespace TRWLASystemMaster.Controllers
         public ActionResult RegisterVol(UserSignUpViewVol USV)
         {
 
-            try
-            {
+        //    try
+       //     {
                 if (ModelState.IsValid)
                 {
                     UserManager UM = new UserManager();
@@ -123,11 +123,11 @@ namespace TRWLASystemMaster.Controllers
                         ModelState.AddModelError("", "Login Name already taken.");
                 }
                 return View();
-            }
-            catch (Exception ex)
-            {
-                return View("Error", new HandleErrorInfo(ex, "Account", "Register"));
-            }
+         //   }
+         //   catch (Exception ex)
+           // {
+                //return View("ErrorSign", new HandleErrorInfo(ex, "Account", "Register"));
+           // }
 
         }
 
