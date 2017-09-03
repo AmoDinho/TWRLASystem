485d240f6c643c22c5cf2fa4c2106e13e934a5c1
Amo Moloko <u14284783@tuks.co.za>
2017-09-03 13:20:08 +0200

    Graduate list
    Adding Generate Grad List :dart:

--- TRWLASystemMaster/TRWLASystemMaster/Controllers/AdminController.cs
+++ TRWLASystemMaster/TRWLASystemMaster/Controllers/AdminController.cs
@@ -3,15 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using TRWLASystemMaster.Models.DB;
 
 namespace TRWLASystemMaster.Controllers
 {
     public class AdminController : Controller
     {
+
+        private TWRLADB_Staging_V2Entities DB = new TWRLADB_Staging_V2Entities();
+
         // GET: Admin
         public ActionResult Index()
         {
-            return View();
+
+            var grad = from g in DB.Students
+                       select g;
+
+
+           
+            grad = grad.Where(h => h.Graduate.Contains('1'));
+                       
+
+            return View(grad);
         }
+
+
     }
 }
