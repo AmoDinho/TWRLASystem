c1073c08a25a82c05626d5647543b645d8d1cd44
Amo Moloko <u14284783@tuks.co.za>
2017-09-26 15:48:35 +0200

    Web Cam + Picture :dart:
    I've added to views foor using the web camera, one with jquery and one with vanilla JS. :ab:

--- TRWLASystemMaster/TRWLASystemMaster/Controllers/AccountController.cs
+++ TRWLASystemMaster/TRWLASystemMaster/Controllers/AccountController.cs
@@ -9,6 +9,7 @@ using TRWLASystemMaster.Models.EntityManager;
 using System.Web.Security;
 using TRWLASystemMaster.Models.DB;
 using DocumentFormat.OpenXml.Spreadsheet;
+using System.IO;
 
 namespace TRWLASystemMaster.Controllers
 {
@@ -36,6 +37,66 @@ namespace TRWLASystemMaster.Controllers
         private TWRLADB_Staging_V2Entities db = new TWRLADB_Staging_V2Entities();
         
         private object confirmationToken;
+
+        public ActionResult CameraBooth()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult PhotoBooth()
+        {
+            return View();
+        }
+
+       
+
+        [HttpGet]
+        public ActionResult Changephoto()
+        {
+            if (Convert.ToString(Session["val"]) != string.Empty)
+            {
+                ViewBag.pic = "http://localhost:26515/WebImages/" + Session["val"].ToString();
+            }
+            else
+            {
+                ViewBag.pic = "../../WebImages/person.jpg";
+            }
+            return View();
+        }
+        public JsonResult Rebind()
+        {
+            string path = "http://localhost:26515/WebImages/" + Session["val"].ToString();
+            return Json(path, JsonRequestBehavior.AllowGet);
+        }
+        public ActionResult Capture()
+        {
+            var stream = Request.InputStream;
+            string dump;
+            using (var reader = new StreamReader(stream))
+            {
+                dump = reader.ReadToEnd();
+                DateTime nm = DateTime.Now;
+                string date = nm.ToString("yyyymmddMMss");
+                var path = Server.MapPath("~/WebImages/" + date + "test.jpg");
+                System.IO.File.WriteAllBytes(path, String_To_Bytes2(dump));
+                ViewData["path"] = date + "test.jpg";
+                Session["val"] = date + "test.jpg";
+            }
+            return View("Index");
+        }
+        private byte[] String_To_Bytes2(string strInput)
+        {
+            int numBytes = (strInput.Length) / 2;
+            byte[] bytes = new byte[numBytes];
+            for (int x = 0; x < numBytes; ++x)
+            {
+                bytes[x] = Convert.ToByte(strInput.Substring(x * 2, 2), 16);
+            }
+            return bytes;
+        }
+
+
 
         //Register Student
         public ActionResult Register()
